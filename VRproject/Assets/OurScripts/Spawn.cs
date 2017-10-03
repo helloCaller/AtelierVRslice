260a28b7c850d0479fb45359a0f0a2c4151c0682
alessia-ip <Alessia@alessias-mbp.ocad.ca>
2017-10-03 10:14:36 -0400

    Folder Structure
    This adds our main scene, our scripts folder, and an assets folder for
    us to use.

---
+++ VRproject/Assets/OurScripts/Spawn.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+
+
+	public GameObject apartmentBlockOne;
+	public GameObject apartmentBlockTwo;
+	public GameObject apartmentBlockThree;
+	public GameObject houseBlock;
+	public GameObject personModel;
+	public GameObject roofBlock;
+
+	public void ApartmentOne(){
+
+		var mousePos = Input.mousePosition;
+		mousePos.z = 300;
+
+		var objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+		Instantiate(apartmentBlockOne, objectPos, Quaternion.identity);
+			
+	}
+
+	public void ApartmentTwo(){
+
+		var mousePos = Input.mousePosition;
+		mousePos.z = 300;
+
+		var objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+		Instantiate(apartmentBlockTwo, objectPos, Quaternion.identity);
+
+	}
+
+	public void ApartmentThree(){
+
+		var mousePos = Input.mousePosition;
+		mousePos.z = 300;
+
+		var objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+		Instantiate(apartmentBlockThree, objectPos, Quaternion.identity);
+
+	}
+
+	public void House(){
+
+		var mousePos = Input.mousePosition;
+		mousePos.z = 300;
+
+		var objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+		Instantiate(houseBlock, objectPos, Quaternion.identity);
+
+	}
+
+	public void Person(){
+
+		var mousePos = Input.mousePosition;
+		mousePos.z = 300;
+
+		var objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+		Instantiate(personModel, objectPos, Quaternion.identity);
+
+	}
+
+	public void Roof(){
+
+		var mousePos = Input.mousePosition;
+		mousePos.z = 300;
+
+		var objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+		Instantiate(roofBlock, objectPos, Quaternion.identity);
+
+	}
+
+
+}
+
