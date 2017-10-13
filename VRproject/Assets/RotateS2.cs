cdb1956ac0d175d0a674f9815224624c7fe20a5c
alessia-ip <Alessia@Alessias-MacBook-Pro.local>
2017-10-13 00:39:05 -0400

    New Scene - Added Assets
    Added in the new scene assets. Contains teleport and controllers. Menu
    buttons have been replaced but don’t yet have the proper items attached
    to them for spawning. Floor jiggle has been fixed.

---
+++ VRproject/Assets/RotateS2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateS2 : MonoBehaviour {
+
+    public float speed = 1f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(speed, 0, 0);
+
+    }
+}
