a2b937b2a8ceae5874cd3f7c9a140bc780564d2a
Luke Garwood <luke_gar@hotmail.com>
2017-10-14 02:53:21 -0400

    added some collision to the ui +spawning of prefabs + animations for dogs and people

---
+++ VRproject/Assets/OurScripts/ObjectSpawn.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawn : MonoBehaviour {
+
+
+	public Transform SpawnPrefab;
+
+	GameObject controller;
+	Collision collision;
+
+	bool spawn = false;
+	Vector3 controllerPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{    
+		
+		if(spawn == false && collision.collider.gameObject.name == "handLeft" | spawn == false && collision.collider.gameObject.name == "handRight")        
+		{
+			spawn = true;
+
+			controller = GameObject.Find (collision.collider.gameObject.name);
+			Debug.Log (controller.transform.position);
+			controllerPosition = new Vector3 (controller.transform.position.x,controller.transform.position.y,controller.transform.position.z);
+			Instantiate(SpawnPrefab,controllerPosition, Quaternion.identity);
+
+		} else {
+			spawn = false;
+		}
+	}
+}
