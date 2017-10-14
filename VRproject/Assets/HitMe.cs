80e55c1c8ee023e83356c490d24f65677db14215
Luke Garwood <luke_gar@hotmail.com>
2017-10-13 23:58:17 -0400

    added destruction, altered machinator script

--- VRproject/Assets/HitMe.cs
+++ VRproject/Assets/HitMe.cs
@@ -16,16 +16,16 @@ public class HitMe : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 
 
 
 
-		
+
 	}
 	void Crumple(){
 		crumple = true;
